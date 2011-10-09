e136e066c28a97e62ff90cc65ebdcdd3801ac3e3
Marc Jeanson <marc@redlinesoftware.com>
2011-10-08 19:39:01 -0500

    initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeerReviewer::Application.config.secret_token = '15e189e80ad5487eefd773b9d29632703c64bedfb2b9071e027cd59ac817395d311a193f7c79934695572f891caf5fd844f961eeb75ae1517589572362786931'
