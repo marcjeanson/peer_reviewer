bec3a5e63e988f4356ee025eb41b578279fad4af
Marc Jeanson <marc@redlinesoftware.com>
2011-10-10 04:24:01 -0500

    Step 7: Adding the sessions#new method.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe SessionsController do
+
+  describe "GET :new" do
+    before { get :new }
+
+    it { should render_template(:new) }
+    it { should respond_with(:success) }
+    it { should_not set_the_flash }
+  end
 end
