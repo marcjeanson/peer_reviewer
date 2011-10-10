1afd21274f038f3b17254c31a8318d0e9829a4ea
Marc Jeanson <marc@redlinesoftware.com>
2011-10-10 06:22:22 -0500

    Step 12: Add the authenticate method for users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :password, :presence => {:on => :create}
+
+  def self.authenticate_for_username(username, password)
+    user = self.where("username = ?", username).first.try(:authenticate, password)
+    user.nil? ? false : user
+  end
+
 end
