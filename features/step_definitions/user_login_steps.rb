12a1d88436ef9b0e9f92d1af424c177222c38288
Marc Jeanson <marc@redlinesoftware.com>
2011-10-10 04:12:52 -0500

    Step 3: Filling in the step definitions

--- features/step_definitions/user_login_steps.rb
+++ features/step_definitions/user_login_steps.rb
@@ -1,11 +1,14 @@
-Given /^the user "([^"]*)" has an account$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given /^the user "([^"]*)" has an account$/ do |username|
+  @user = User.create! username: username, password: 'password'
 end
 
 When /^he logs in$/ do
-  pending # express the regexp above with the code you wish you had
+  visit(login_path)
+  fill_in('Username', with: @user.username)
+  fill_in('Password', with: @user.password)
+  click_on('Login')
 end
 
-Then /^he should see "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^he should see "([^"]*)"$/ do |message|
+  page.should have_content(message)
 end
