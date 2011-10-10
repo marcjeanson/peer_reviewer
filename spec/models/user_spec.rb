f19df3df465ab831fed927041037ec93f8d4abba
Marc Jeanson <marc@redlinesoftware.com>
2011-10-10 06:22:22 -0500

    Step 12: Add the authenticate method for users.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 
 describe User do
   it { should validate_presence_of(:password) }
+
+  it "authenticates a user with valid username and password returning a valid user" do
+    @user = User.create! :username => 'marc', :password => 'password'
+    User.authenticate_for_username(@user.username, 'password').should eq(@user)
+  end
 end
