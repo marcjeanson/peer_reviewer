d8acaee882b7cfdf0f8ec3ebd9f2719e83979bb1
Marc Jeanson <marc@redlinesoftware.com>
2011-10-10 06:09:52 -0500

    Step 11: Authenticate the user and store the user_id in the session.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authenticate_for_username(params[:username], params[:password])
+    session[:user_id] = user.id
     redirect_to root_path
   end
 end
