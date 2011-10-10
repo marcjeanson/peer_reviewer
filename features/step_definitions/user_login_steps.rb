fbcac98aac23266a7ffaf5efad173f6463ed37e2
Marc Jeanson <marc@redlinesoftware.com>
2011-10-10 04:06:54 -0500

    Step 2: Create the user login step definitions based on the output from cucumber.

---
+++ features/step_definitions/user_login_steps.rb
@@ -0,0 +1,11 @@
+Given /^the user "([^"]*)" has an account$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^he logs in$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^he should see "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
