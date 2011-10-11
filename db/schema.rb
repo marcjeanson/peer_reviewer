4f069be3f9e3cc353169f2813526f953299de99f
Marc Jeanson <marc@redlinesoftware.com>
2011-10-10 22:36:28 -0500

    Step 7: Adding the names to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111011030335) do
+ActiveRecord::Schema.define(:version => 20111011032445) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20111011030335) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
