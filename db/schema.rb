02f1c901f1adae9ba805faaf54581fc798036efd
Marc Jeanson <marc@redlinesoftware.com>
2011-10-10 22:04:34 -0500

    Step 4: Add the role attribute to users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111010075501) do
+ActiveRecord::Schema.define(:version => 20111011030335) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
 end
