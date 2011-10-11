c5a1f220bae6b2092e541e64b106846304e84291
Marc Jeanson <marc@redlinesoftware.com>
2011-10-10 21:55:09 -0500

    Step 2: generated step definitions from cucumber

---
+++ features/step_definitions/user_management_steps.rb
@@ -0,0 +1,16 @@
+Given /^a logged in admin user$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the following users:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I visit the user admin page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see all the users$/ do
+  pending # express the regexp above with the code you wish you had
+end
