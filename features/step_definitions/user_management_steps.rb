622a2a00c1590d2f2118140c5a1a45740a1d5ea7
Marc Jeanson <marc@redlinesoftware.com>
2011-10-10 22:10:01 -0500

    Step 5: forgot to actually login the admin user after creating the user.

--- features/step_definitions/user_management_steps.rb
+++ features/step_definitions/user_management_steps.rb
@@ -1,5 +1,6 @@
 Given /^a logged in admin user$/ do
   @admin = User.create! username: 'admin', password: 'password', role: 'admin'
+  login(@admin.username, @admin.password)
 end
 
 Given /^the following users:$/ do |table|
