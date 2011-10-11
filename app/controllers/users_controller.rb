b377af3b751fddcd4f6acd89479c682d1d0e404d
Marc Jeanson <marc@redlinesoftware.com>
2011-10-11 03:08:38 -0500

    Step 6: Adding the create action for the users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      redirect_to users_path, notice: "User was successfully created!"
+    else
+      render :new
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe UsersController do
+  let(:user) { mock_model(User) }
   let(:users) { 3.times.collect { stub_model(User) } }
 
   describe "GET :index" do
@@ -16,8 +17,6 @@ describe UsersController do
   end
 
   describe "GET :new" do
-    let(:user) { stub }
-
     before do
       User.stub(:new).and_return(user)
       get :new
@@ -27,6 +26,30 @@ describe UsersController do
     it { should render_template(:new) }
     it { should_not set_the_flash }
     it { should assign_to(:user).with(user) }
+  end
+
+  describe "POST :create" do
+    before { User.stub(:new).and_return(user) }
+
+    context "user has valid data" do
+      before do
+        user.stub(:save).and_return(true)
+        post :create, :user => { :username => 'dvader', :password => 'password' }
+      end
+
+      it { should redirect_to(users_path) }
+      it { should set_the_flash.to(notice: "The user was created!") }
+    end
+
+    context "user has invalid data" do
+      before do
+        user.stub(:save).and_return(false)
+        post :create, :user => { :username => 'dvader', :password => 'password' }
+      end
+
+      it { should render_template(:new) }
+
+    end
   end
 
 end
